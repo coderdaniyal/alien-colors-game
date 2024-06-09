701e3719a13f54ed2bbfd52e3becace388f6cba9
codewithdaniyal <167603648+codewithdaniyal@users.noreply.github.com>
2024-06-09 23:48:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var alien_color = 'Green';
+if (alien_color === 'Green') {
+    console.log('The player just earned 5 points');
+}
+else {
+    console.log();
+}
+var alienColor = 'Red';
+if (alienColor === 'Green') {
+    console.log('The player just earned 5 points');
+}
+else {
+    console.log();
+}
